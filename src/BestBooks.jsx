fb1ab98114f2119a7ec6a883669ccc3632aeac5e
Brendan <bgmcmullen@comcast.net>
2024-04-03 20:54:02 -0400

    added book state

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,4 +1,6 @@
+
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,7 +10,12 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  fetchBooks() {
+    axios.get('http://localhost:3001/books').then(response => {
+      this.setState({ bookData: response.data});
+    });
+
+  }
 
   render() {
 
@@ -16,6 +23,7 @@ class BestBooks extends React.Component {
 
     return (
       <>
+      <p>{this.state}</p>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
