cb9f7715852bb049649d57e3b4705294db615022
Brendan <bgmcmullen@comcast.net>
2024-04-04 21:42:51 -0400

    added create function

---
+++ src/BookModal.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+function  BookModal(props){
+
+
+  return(      
+  <Modal show={props.show} onHide={props.handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Add a Book!</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control value={props.title} onChange={(e) => props.setTitle(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={3} value={props.description} onChange={(e) => props.setDescription(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Status</Form.Label>
+          <Form.Control value={props.status} onChange={(e) => props.setStatus(e.target.value)} />
+        </Form.Group>
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={props.handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={props.handleSaveChanges}>
+        Save Changes
+      </Button>
+    </Modal.Footer>
+  </Modal>
+
+
+  );
+}
+
+export default BookModal;
