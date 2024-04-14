cffd76693c66df5b8b939500fa1fee4019fbbaf3
Brendan <bgmcmullen@comcast.net>
2024-04-13 22:08:13 -0400

    authentication

--- src/BookModal.jsx
+++ src/BookModal.jsx
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
+
 function  BookModal(props){
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const AUTH_CLIENT_ID = import.meta.env.VITE_CLIENT;
+const AUTH_DOMAIN = import.meta.env.VITE_DOMAIN;
+
+
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain = {AUTH_DOMAIN}
+    clientId = {AUTH_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>
 );
